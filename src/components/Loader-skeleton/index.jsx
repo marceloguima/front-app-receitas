6e3a42b9f8b6186bc100d121d366d4849109a15e
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2026-01-27 23:14:59 -0400

    ajuste no skeleton

--- src/components/Loader-skeleton/index.jsx
+++ src/components/Loader-skeleton/index.jsx
@@ -4,13 +4,16 @@ import { MdImage } from "react-icons/md";
 
 const LoaderSkeletonCard = () => {
     return (
-        <div className="card skeleton-card">
+        <div className="skeleton-card">
             <div className="skeleton skeleton-img">
                 <MdImage className="skeleton-icon" />
             </div>
-            <div className="skeleton skeleton-title" ></div>
-       
-            <div className="skeleton skeleton-btn"></div>
+            <div className="skeleton skeleton-title"></div>
+
+            <div className="skeleton-info">
+                <div className="skeleton skeleton-temp"></div>
+                <div className="skeleton skeleton-dif"></div>
+            </div>
         </div>
     );
 };

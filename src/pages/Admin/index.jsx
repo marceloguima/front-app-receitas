0a3ea0d97636fdd7f8de8ff2d7a6c67e5c13b7f6
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2026-02-02 23:22:49 -0400

    cria campo para ingredientes, para adicionar varios ingredientes separadamente

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -2,13 +2,16 @@ import React, { Children } from "react";
 import "./styles.css";
 import Botao from "../../components/Botao";
 import Modal from "../../components/Modal";
+
+import FormAdmin from "../../components/Form-cadastro-admin";
+import CampoEntradaAdmin from "../../components/Campo-entrada-admin";
 import { useState } from "react";
 
 // icones
 import { PiChefHatFill } from "react-icons/pi";
 import { TfiAnnouncement } from "react-icons/tfi";
-import FormAdmin from "../../components/Form-cadastro-admin";
-import CampoEntradaAdmin from "../../components/Campo-entrada-admin";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { IoMdAdd } from "react-icons/io";
 
 const TelaAdmin = () => {
     const [showModal, setShowModal] = useState(false);
@@ -16,8 +19,6 @@ const TelaAdmin = () => {
     const fechaModal = () => {
         setShowModal(false);
     };
-
-
 
     const mostrarModalReceita = () => {
         setShowModal(
@@ -61,17 +62,44 @@ const TelaAdmin = () => {
                             <option value="chef">Nível Chef</option>
                         </CampoEntradaAdmin>
                     </div>
-                    <CampoEntradaAdmin
-                        textLabel="Ingredientes"
-                        placeholder="Ex: 2 xícaras de farinha"
-                    />
+                    <div className="container_ingredientes">
+                        <h4>Ingredientes</h4>
+                        <div className="campo-ingredientes">
+                            <div className="ingrediente">
+                                <CampoEntradaAdmin
+                                    textLabel="Nome do ingrediente"
+                                    placeholder="Ex: Farinha de trigo"
+                                />
+
+                                <CampoEntradaAdmin
+                                    textLabel="Unidade de medida"
+                                    placeholder="Ex: Chicara, colher, ml..."
+                                />
+
+                                <CampoEntradaAdmin
+                                    tipo="number"
+                                    textLabel="Quantidade"
+                                    placeholder="Ex: 2"
+                                />
+                                <Botao variant="btn-p" type="button">
+                                    <RiDeleteBin6Line />
+                                </Botao>
+                            </div>
+                            <Botao type="button" variant="add-ingrediente">
+                                <span>
+                                    <IoMdAdd />
+                                </span>
+                                Ad. ingrediente
+                            </Botao>
+                        </div>
+                    </div>
                     <CampoEntradaAdmin
                         tipo="textarea"
                         textLabel="Modo de preparo"
                         placeholder="Descreva sua receita..."
                         rows={5}
                     />{" "}
-                     <CampoEntradaAdmin
+                    <CampoEntradaAdmin
                         textLabel="Imagem"
                         placeholder="Insira a URL da imagem"
                     />
@@ -92,9 +120,6 @@ const TelaAdmin = () => {
             </Modal>,
         );
     };
-
-
-  
 
     return (
         <div className="container-admin">
